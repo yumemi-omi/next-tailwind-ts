8998e2c5dac94da38496f471429512d6b2b8ad7a
yumemi-omi <hiro.041310402.kt@gmail.com>
2020-11-16 00:41:00 +0900

    chore: tailwind have already included normalize.css so we can remove ress.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import 'ress';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
